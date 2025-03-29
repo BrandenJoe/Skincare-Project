bec71d98a76358d93de5d0085623d83497393e4c
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-03-29 14:52:58 -0500

    .
    .

--- skincare-project/src/App.jsx
+++ skincare-project/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="text-3xl text-center text-blue-500 font-bold">
-      Hello world
-    </div>
+    <header className="text-3xl text-center text-blue-500 font-bold">
+    Sophisticated Skincare
+    </header>
   )
 }
 
