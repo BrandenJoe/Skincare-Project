428b21c7ddefdce354965d4c474bddc7ce2d5c97
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-03-29 14:36:10 -0500

    first commit

---
+++ skincare-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-3xl text-center text-blue-500 font-bold">
+      Hello world
+    </div>
+  )
+}
+
+export default App
