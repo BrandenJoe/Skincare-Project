24a7bb93bdaaa90cb03bf3663286c09ada9a22cf
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-03-30 16:41:22 -0500

    added the left and right rectangle and buttons

--- skincare-project/src/App.jsx
+++ skincare-project/src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import leftrectangle from "./assets/leftrectangle.png";
+import rightrectangle from "./assets/rightrectangle.png";
 function App() {
   return (
-    <div className='flex items-center justify-center h-screen'>
-    <h1 className="text-center font-roobert-trial font-light text-[128px] leading-[120px] tracking-[-0.07em]">
-    Sophisticated 
-    <span className='block'>Skincare</span>
-    </h1>
+    <div className="flex h-screen items-center">
+      <div className=" relative flex-shrink-0">
+        <img src={leftrectangle} alt ="Right Rectangle" className=" h-auto" />
+        <button className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[#1A1B1C] px-4 py-2 rounded">
+         Discover A.I.
+         </button>
+      </div>
+
+      <div className=" flex flex-col items-center justify-center h-[71dvh] md:fixed md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+      <div className="flex items-center justify-center flex-grow">
+        <h1 className="text-center font-roobert-trial font-light text-[60px] lg:text-[100px] leading-[120px] tracking-[-0.07em]">
+          Sophisticated
+          <span className="block">Skincare</span>
+        </h1>
+      </div>
+      </div>
+
+      <div className="flex-shrink-0 ml-auto">
+        <img src={rightrectangle} alt ="Right Rectangle" className=" h-auto" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- skincare-project/src/nav.jsx
+++ skincare-project/src/nav.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function nav() {
   return (
-    <nav className='flex items-center justify-between p-6'>
-       <button className='text-600 text-lg font-medium'>skinstric</button>
-      <div className='text-gray-700 text-lg font-medium'>
-        <a href="#" className='hover:text-gray-900'>[ intro ]</a>
-      </div>
-       <button  className='text-gray-700 text-lg font-medium'>Enter Code</button>
+    <nav className="flex items-center justify-between p-6">
+      <header className="text =[#1A1B1C] font-roobert-trial font-semibold text-[14px] leading-[16px] tracking-[-0.02em] uppercase">
+        skinstric 
+        <span className="opacity-60 text-[14px] leading-[16px] tracking-[-0.02em] uppercase">   [ intro ]</span>
+      </header>
+      <button className=" text-white font-roobert-trial font-semibold text-[10px] leading-[16px] tracking-[-0.02em] uppercase bg-black rounded w-[80px] h-[32px] .py-2 .px-4 cursor-pointer ">
+        Enter Code
+      </button>
     </nav>
-  )
+  );
 }
 
-export default nav
+export default nav;
